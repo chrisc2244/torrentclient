29046fa3c21bc7b2a4dc3c189628e1341506e1a5
chrisc2244 <101891545+chrisc2244@users.noreply.github.com>
2025-01-13 16:48:48 -0500

    BencodingDecoder and TorrentInfo comments

---
+++ BencodingDecoder.java
@@ -0,0 +1,8 @@
+public class BencodingDecoder {
+
+    // Take in a Byte array, leave loading the file and sending the byte array to another class
+    // Iterate through the byte array, maybe decode to a map, something I can easily do next step from
+    // Extract TorrentInfo fields
+    // do what with this after I get it..?
+
+}
---
+++ TorrentInfo.java
@@ -0,0 +1,31 @@
+public class TorrentInfo {
+    /*
+    https://wiki.theory.org/BitTorrentSpecification#Metainfo_File_Structure
+
+    info
+    single file mode:
+        name
+        length
+        md5sum
+
+    multiple file mode:
+        name
+        files
+            length
+            md5sum
+            path
+
+    announce
+    announce-list
+    creation date
+    comment
+    created by
+    encoding
+
+    ____
+    piece length
+    pieces
+    private
+     */
+
+}
