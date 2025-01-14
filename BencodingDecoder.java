15751e5ef006f1f76edab59a6b88ab42f903f02e
Chris Castro <cmc-castro@hotmail.com>
2025-01-14 05:05:24 -0500

    early decode but really bad

--- BencodingDecoder.java
+++ BencodingDecoder.java
@@ -3,6 +3,68 @@ public class BencodingDecoder {
     // Take in a Byte array, leave loading the file and sending the byte array to another class
     // Iterate through the byte array, maybe decode to a map, something I can easily do next step from
     // Extract TorrentInfo fields
-    // do what with this after I get it..?
+    // do what with this after I get it?
+
+    /*
+    i integers e, can be negative
+    bytestring length:contents
+    l listitems e
+    d dictionarypairs e
+     */
+
+
+    /*
+    iterate through the byte array
+    if we encounter a character we recognize
+    we call the decode method for that char
+    then we read the number of bytes till the next :
+    that tells us how many to iterate, and helps us avoind recognizing : in the middle of a field
+    if we encounter
+
+
+    for ints we can iterate through until we reach an e
+    for lists we can iterate through
+     */
+
+    public static TorrentInfo Decode(byte[] input) throws Exception {
+
+        final TorrentInfo.TorrentInfoBuilder torrentInfo = new TorrentInfo.TorrentInfoBuilder();
+        long counter = 0;
+
+        for (byte b : input) {
+
+            switch (b) {
+                case 'i':
+                    DecodeInteger(counter);
+                    break;
+                case 'l':
+                    DecodeList(counter);
+                    break;
+                case 'd':
+                    DecodeDict(counter);
+                    break;
+                default:
+                    throw new Exception("Valid ASCII char not detected.");
+
+            }
+        }
+        return torrentInfo.build();
+    }
+
+    private static long DecodeDict(long counter) {
+        System.out.println("IM INSIDE DECODEDICT" + counter);
+        return counter;
+    }
+
+    private static long DecodeList(long counter) {
+        System.out.println("IM INSIDE DECODELISTTTT" + counter);
+        return counter;
+    }
+
+    private static long DecodeInteger(long counter) {
+        System.out.println("IM INSIDE decodeint" + counter);
+        return counter;
+    }
+
 
 }
--- TorrentInfo.java
+++ TorrentInfo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class TorrentInfo {
     /*
     https://wiki.theory.org/BitTorrentSpecification#Metainfo_File_Structure
@@ -22,10 +24,109 @@ public class TorrentInfo {
     created by
     encoding
 
-    ____
+    ____info
     piece length
     pieces
     private
      */
+
+    public String announce;
+    public ArrayList<String> announceList = new ArrayList<String>();
+    public String creationDate;
+    public String comment;
+    public String createdBy;
+    public String encoding;
+    public long length;
+    public String name;
+    public long pieceLength;
+    public ArrayList<String> pieces = new ArrayList<>();
+    public String privacy;
+
+    private TorrentInfo(TorrentInfoBuilder torrentInfoBuilder) {
+        this.announce = torrentInfoBuilder.announce;
+        this.announceList = torrentInfoBuilder.announceList;
+        this.creationDate = torrentInfoBuilder.creationDate;
+        this.comment = torrentInfoBuilder.comment;
+        this.createdBy = torrentInfoBuilder.createdBy;
+        this.encoding = torrentInfoBuilder.encoding;
+        this.length = torrentInfoBuilder.length;
+        this.pieceLength = torrentInfoBuilder.pieceLength;
+        this.pieces = torrentInfoBuilder.pieces;
+        this.privacy = torrentInfoBuilder.privacy;
+    }
+
+
+    public static class TorrentInfoBuilder {
+
+        public String announce;
+        public ArrayList<String> announceList = new ArrayList<>();
+        public String creationDate;
+        public String comment;
+        public String createdBy;
+        public String encoding;
+        public long length;
+        public String name;
+        public long pieceLength;
+        public ArrayList<String> pieces = new ArrayList<>();
+        public String privacy;
+
+        public TorrentInfoBuilder() {}
+
+        public void setAnnounce(String announce) {
+            this.announce = announce;
+        }
+
+        public void setAnnounceList(ArrayList<String> announceList) {
+            this.announceList = announceList;
+        }
+
+        public void setComment(String comment) {
+            this.comment = comment;
+        }
+
+        public void setCreationDate(String creationDate) {
+            this.creationDate = creationDate;
+        }
+
+        public void setCreatedBy(String createdBy) {
+            this.createdBy = createdBy;
+        }
+
+        public void setEncoding(String encoding) {
+            this.encoding = encoding;
+        }
+
+        public void setLength(long length) {
+            this.length = length;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setPieceLength(long pieceLength) {
+            this.pieceLength = pieceLength;
+        }
+
+        public void setPieces(ArrayList<String> pieces) {
+            this.pieces = pieces;
+        }
+
+        public void setPrivacy(String privacy) {
+            this.privacy = privacy;
+        }
+
+        public TorrentInfo build() {
+            return new TorrentInfo(this);
+        }
+    }
+
+
+
+
+
+
+
+
 
 }
